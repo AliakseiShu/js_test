bc578c2e24a2ab20f592bcc6d89acfeecff239ef
Aliaksei Shulha <aliakseishulha2021@gmail.com>
2022-01-13 15:06:56 +0300

    created new function

---
+++ task1.js
@@ -0,0 +1,8 @@
+function shortcut(string){
+ const vowels = /[aeiou]+/g;
+ return string.replace(vowels, "");
+}
+console.log ("hello")
+console.log ("codewars")
+console.log ("goodbye")
+console.log ("HELLO")
